c9e73d4468510b04c64ab97b9bda1f03d5ae4298
Artem Lira <artemlira84@gmail.com>
2023-02-26 19:34:30 +0200

    create app my-portfolio

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:react-hooks/recommended'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "linebreak-style": ["error", "windows"],
+    "import/order": [
+      "error",
+      {
+        "groups": [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type"
+        ]
+      }
+    ]
+  },
+  ignorePatterns: [".eslintrc.cjs", "postcss.config.js", "vite.config.js"],
+};
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      My portfolio
+    </div>
+  );
+}
+
+export default App;
