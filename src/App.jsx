5820c88ae6d6e69fd830738ea5e2249c782b1357
Artem Lira <artemlira84@gmail.com>
2023-02-27 02:31:26 +0200

    removed the comments before the errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,5 +1,4 @@
 import React, { useState, createContext } from 'react';
-// import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 export const MyContext = createContext();
