8271980f95f9cb87bf1fe31c3827fab479739515
Artem Lira <artemlira84@gmail.com>
2023-03-16 01:15:22 +0200

    added a Netlify form

--- src/components/Page_Home/Contacts/Contacts.jsx
+++ src/components/Page_Home/Contacts/Contacts.jsx
@@ -15,27 +15,28 @@ function Contacts() {
           {t('contacts_title')}
         </h2>
         <div className={styles.container}>
-          <div className="content">
-            <p className="text">
+          <div className={styles.content}>
+            <p className={styles.text}>
               {t('contacts_text')}
             </p>
-            <div className="telegram">
+            <div className={styles.telegram}>
               <Telegram />
               <a href="https://t.me/Artem_Lira" target="_blank" rel="noreferrer">@Artem_Lira</a>
             </div>
-            <div className="email">
+            <div className={styles.email}>
               <Email />
               <a href="mailto:artemliradev@gmail.com">artemliradev@gmail.com</a>
             </div>
           </div>
-          <form action="" className="form">
-            <div className="label">
-              <input type="text" className="inputName" placeholder={t('contacts_inputName')} />
-              <input type="text" className="inputEmail" placeholder={t('contacts_inputEmail')} />
+          <form name="contacts" data-netlify="true" method="POST" className={styles.form}>
+            <h4 className={styles.formTitle}>{t('contacts_formTitle')}</h4>
+            <div className={styles.label}>
+              <input type="text" className={styles.inputName} placeholder={t('contacts_inputName')} />
+              <input type="text" className={styles.inputEmail} placeholder={t('contacts_inputEmail')} />
             </div>
-            <input type="text" className="inputTitle" placeholder={t('contacts_inputTitle')} />
-            <input type="text" className="message" placeholder={t('contacts_message')} />
-            <button type="submit" className="button">{t('contacts_button')}</button>
+            <input type="text" className={styles.inputTitle} placeholder={t('contacts_inputTitle')} />
+            <input type="text" className={styles.message} placeholder={t('contacts_message')} />
+            <button type="submit" className={styles.button}>{t('contacts_button')}</button>
           </form>
         </div>
       </div>
