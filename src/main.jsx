f57cd2c62b5280d64114e49e3080ffec154457a0
Lira Artem <artemlira84@gmail.com>
2023-05-21 02:07:31 +0300

    added a spinner for download

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { FadeLoader } from 'react-spinners';
 import App from './App';
 import './styles/index.scss';
 import ScrollToTop from './components/ScrollToTop';
 import Context from './components/Context';
 import './i18n';
 
+const override = {
+  display: 'block',
+  margin: '0 auto',
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Suspense fallback={<div className="loading">Loading...</div>}>
+    <Suspense fallback={<FadeLoader cssOverride={override} color="#c778dd" />}>
       <BrowserRouter>
         <Context>
           <ScrollToTop />
