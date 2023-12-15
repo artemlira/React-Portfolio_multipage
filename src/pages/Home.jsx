a4ef823075830f0b7f046a797ee93b154f43da10
Artem Lira <artemlira84@gmail.com>
2023-12-15 11:54:02 +0200

    add clarity

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Hero from '../components/Page_Home/Hero/Hero';
-import Projects from '../components/Page_Home/Projects/Projects';
-import Skills from '../components/Page_Home/Skills/Skills';
-import AboutMe from '../components/Page_Home/AboutMe/AboutMe';
-import Contacts from '../components/Page_Home/Contacts/Contacts';
+import React from "react";
+import Hero from "../components/Page_Home/Hero/Hero";
+import Projects from "../components/Page_Home/Projects/Projects";
+import Skills from "../components/Page_Home/Skills/Skills";
+import AboutMe from "../components/Page_Home/AboutMe/AboutMe";
+import Contacts from "../components/Page_Home/Contacts/Contacts";
 
 function Home() {
   return (
